36fb39c95bc5aa2db0eb132a00f8da8b3e5d303e
IsraAlmaraz <israelalmaraz537@gmail.com>
2023-10-16 18:22:49 -0600

    con prints

--- pg2_20231016.py
+++ pg2_20231016.py
@@ -1,46 +1,38 @@
+import random
 from gpiozero import LED, Button
 import time
 import psutil
 
-Verde=LED(17)
-Rojo=LED(18)
-Azul=LED(19)
+Verde = LED(17)
+Rojo = LED(18)
+Azul = LED(19)
 
 while True:
     cpu_percent = psutil.cpu_percent(interval=1)
-    
-    if cpu_percent<=10:
-        Verde.on()
-        Rojo.off()
-        Azul.off()
-    if cpu_percent>10 and cpu_percent<=20:
-        Verde.off()
-        Rojo.on()
-        Azul.off()
-    if cpu_percent>20:
-        Verde.off()
-        Rojo.off()
-        Azul.on()
 
-#2do Programa Lectura de Temperatura
-import time
-import random  
+    if cpu_percent <= 10:
+        print("Verde On, Amarillo Off, Rojo Off")
+
+    if cpu_percent > 10 and cpu_percent <= 20:
+        print("Verde Off, Amarillo ON, Rojo Off")
+    if cpu_percent > 20:
+
+        print("Verde Off, Amarillo Off, Rojo parpadeando")
+
+# 2do Programa Lectura de Temperatura
 
 
 temperaturas = []
 
 while True:
 
-    temperatura_simulada = random.uniform(20, 30) 
+    temperatura_simulada = random.uniform(20, 30)
     temperaturas.append(temperatura_simulada)
-
 
     if len(temperaturas) > 10:
         temperaturas.pop(0)
 
-
     promedio_temperatura = sum(temperaturas) / len(temperaturas)
     print(f'Promedio de temperatura: {promedio_temperatura:.2f}°C')
 
-
-    time.sleep(10) 
+    time.sleep(10)
